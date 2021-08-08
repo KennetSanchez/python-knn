c47dbf9b7ab84c02800b8d63e66ae94530034508
KennetSanchez <kesasaro@gmail.com>
2021-08-08 13:19:46 -0500

    Graphics working, algorithm working (Only integer numbers).
    * IMPORTANT: Right now the program can only take integer numbers.
    
    * The program is working fine.
    
    * I'll try to make it work with decimal numbers too in the next versions.

--- BE.py
+++ BE.py
@@ -18,16 +18,19 @@ class KNN():
           K: the number of nearest neighboors
         """
         self.k = k
+        # Dimensions used (2, x and y)
+        self.dimensions = 2
+        self.classPosition = 4
 
-    #Recibe el vector a evaluar
+    # Recibe el vector a evaluar
     def get_k_nearest_neighboors(self, datapoint):
         """
         Gets the k-nearest neighboors of a given datapoint
         Argunments:
           datapoint: numpy.array, a row vector
         Returns:
-          indices: list, indices corresponding with the k datapoints in self.X most 
-                   similar to datapoint   
+          indices: list, indices corresponding with the k datapoints in self.X most
+                   similar to datapoint
         """
 
         distances = []  # distances between the matrix and the datapoint
@@ -38,36 +41,48 @@ class KNN():
         for i in range(size):
             vector_of_the_matrix = self.data[i]
             np_vector_of_the_matrix = np.array(vector_of_the_matrix)
-            two_vectors_difference = self.calculate_distance(datapoint, np_vector_of_the_matrix)
+            two_vectors_difference = self.calculate_distance(
+                self, datapoint, np_vector_of_the_matrix)
             distances.append(two_vectors_difference)
 
         distances = np.array(distances)
-        indices = distances.argsort() #Retorna los ínidices de las distancias de menor a mayor
+        # Retorna los ínidices de las distancias de menor a mayor
+        indices = distances.argsort()
         k_indices = []
-        
+
         for i in range(self.k):
             k_indices.append(indices[i])
-            
+
         return k_indices
 
-    #Recibe el vector nuevo que se está evaluando, y el vector de la matriz
+    # Recibe el vector nuevo que se está evaluando, y el vector de la matriz
     def calculate_distance(self, datapoint1, datapoint2):
         """
-        Calculates the euclidean 
+        Calculates the euclidean
         Arguments:
           datapoint1: numpy.array, first datapoint. It's the row vector we want to compare with the others.
           datapoint2: numpy.array, second datapoint
         Returns:
           Distance between the given datapoints
         """
+
+        # Parse to x dimesions of the main matrix
+
+        formatedDTP = []
+
         
-        if isinstance(datapoint1, np.ndarray) and isinstance(datapoint2, np.ndarray):
-            array3 = np.subtract(datapoint2, datapoint1)
-            return np.linalg.norm(array3)
+        for i in range(self.dimensions):
+            formatedDTP.append(datapoint2[i])
+
+        formatedDTP = np.array(formatedDTP)
+        if(len(datapoint1) != len(datapoint2)):
+              array3 = np.subtract(formatedDTP, datapoint1)
         else:
-            raise ValueError(" Datatype not valid")
+              array3 = np.subtract(datapoint2, datapoint1)
+        return np.linalg.norm(array3)
 
     #
+
     def fit(self, main_matrix, y):  # main_matrix antes era X
         """
         Train the model, i.e., allocate the dictionary with features by datapoint 
@@ -90,15 +105,39 @@ class KNN():
         """
         preds = []
         for datapoint in X:
-            indices = self.get_k_nearest_neighboors(datapoint)
+            index = self.get_k_nearest_neighboors(self, datapoint)
             # Obtener los indices de las clases
-            classes = np.array([self.classes[idX] for idX in indices])
+            classes = np.array([self.classes[idX] for idX in index])
             # Obtener la clase mas frecuente de los vecinos mas cercanos
             counts = np.bincount(classes)
-            #Cuenta cuantos elementos hay de cada número, y los pone en orden (1, 3, 3, 4) --> (1, 0, 2, 1)
+            # Cuenta cuantos elementos hay de cada número, y los pone en orden (1, 3, 3, 4) --> (1, 0, 2, 1)
             predicted_class = np.argmax(counts)
-            #Retorna el índice del mayor valor
+            # Retorna el índice del mayor valor
             preds.append(predicted_class)
         return np.array(preds)
 
+    def getClass(data):
+      pred = []
+      
+      for i in range(len(data)):
+        pred.append(KNN.classes[data[i]])
+      pred = np.array(pred) 
+      return pred
+      
+          
 
+if __name__ == '__main__':
+  iris = load_iris()
+  X = iris.data
+  Y = iris.target
+  X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3)
+  data, classes = load_iris(return_X_y=True)
+  k = 3
+  KNN.__init__(KNN, k)
+  KNN.fit(KNN, X, Y)
+  newData = [8, 8]
+  preds = KNN.get_k_nearest_neighboors(KNN, newData)
+  print(X)
+  print(Y)
+  for i in range(k):
+    print(X[preds[i]])
--- Visualizer.py
+++ Visualizer.py
@@ -1,3 +1,4 @@
+from BE import KNN
 import numpy as np
 from sklearn.model_selection import train_test_split
 import numpy as np
@@ -6,46 +7,100 @@ from sklearn.datasets import load_iris
 import plotly.express as px
 import plotly.graph_objects as go
 
-def askData():
-    print("Type the sepal width:")
-    x = int(input())
-    print("Type the sepal length")
-    y = int(input())
-
-    newData = [x, y]
-    return newData
-
-
-def start():
-    iris = load_iris()
-    X = iris.data
-    Y = iris.target
-    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3)
+class visualizer():
     
-    """
-    print(type(X_train))
-    print(X_train)
-    print(type(y_train))
-    print(y_train)
-    """
-    data, classes = load_iris(return_X_y=True)
-    
-    #Graphic    
-    df = px.data.iris()
-    df = df[["sepal_width", "sepal_length", "species"]]
-    fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species")
-    fig.show()
-    return df
-          
+    def askData():
+        print("Type the sepal width:")
+        x = int(input())
+        print("Type the sepal length")
+        y = int(input())
 
-def addDot(df):
-    newData = askData()
+        #The classes matrix works with [y, x] so i have to change the order of my array
+        classesData = [y, x]
+        
+        #The position works as usual, so i need it in the normal order
+        positionData = [x, y]
+        data = [classesData, positionData]
+        
+        return data
 
-    newDyc = {"sepal_width": newData[0], "sepal_length": newData[1], "species": "setosa"}
-    df = df.append(newDyc, ignore_index=True)
-    fig2 = px.scatter(df, x="sepal_width", y="sepal_length", color="species")
-    fig2.show()
+
+    def __init__(self):
+        iris = load_iris()
+        self.Xvar = iris.data
+        self.Yvar = iris.target
+
+        X_train, X_test, y_train, y_test = train_test_split(visualizer.Xvar, visualizer.Yvar, test_size=0.3)
+
+        data, classes = load_iris(return_X_y=True)
+        
+    def start():      
+        k = 3
+        KNN.__init__(KNN, k)
+        KNN.fit(KNN, visualizer.Xvar, visualizer.Yvar)
+ 
+        
+        data, classes = load_iris(return_X_y=True)
+        
+        #Graphic    
+        df = px.data.iris()
+        df = df[["sepal_width", "sepal_length", "species"]]
+        fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species")
+        fig.show()
+        return df
+            
+
+    def addDot(df, classData, positionData):
+
+        
+        #Para los cercanos
+        vecinos = KNN.get_k_nearest_neighboors(KNN, classData)
+        vecinos = np.array(vecinos)
+        Y = KNN.predict(KNN, visualizer.Xvar)
+        
+        preds = []
+        
+        #Classes numeration
+        classes = [0, 1, 2]
+        
+        #Classes count
+        count = [0, 0, 0]
+        count = np.array(count)
+        for i in range(len(vecinos)):
+            preds.append(Y[vecinos[i]])
+        
+        for i in range(len(preds)):
+            for j in range(len(classes)):
+                if(preds[i] == classes[j]):
+                    count[j] += 1
+                     
+        mostRepitedValue = 0
+        
+        for i in range(len(count)-1):
+            if(count[i] > count[i+1]):
+                mostRepitedValue = i
+
+        detectedSpecie = preds[mostRepitedValue]
+        
+        if(detectedSpecie == 0):
+            detectedSpecie = "setosa"
+        elif(detectedSpecie == 1):
+            detectedSpecie = "versicolor"
+        elif(detectedSpecie == 2):
+            detectedSpecie = "virginica"
+        else:
+            print("error")
     
+        newDyc = {"sepal_width": positionData[0], "sepal_length": positionData[1], "species": detectedSpecie}
+        df = df.append(newDyc, ignore_index=True)
+        fig2 = px.scatter(df, x="sepal_width", y="sepal_length", color="species")
+        fig2.show()
+        
 if __name__ == '__main__':
-    df = start()
-    addDot(df)
+    visualizer.__init__(visualizer)
+    df = visualizer.start()
+    data = visualizer.askData()
+    visualizer.addDot(df, data[0], data[1])
+    
+
+    
