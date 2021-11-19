d935d82e6328b9edd387679b1216ea02be918aba
KennetSanchez <kesasaro@gmail.com>
2021-11-19 12:38:39 -0500

    Bug fixed
    The addition of a new dot problem have been fixed.

--- BE.py
+++ BE.py
@@ -1,8 +1,8 @@
 from numpy.core.fromnumeric import size
-from sklearn.model_selection import train_test_split
+#from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
-from sklearn.datasets import load_iris
+#from sklearn.datasets import load_iris
 """
 import plotly.express as px
 import plotly.graph_objects as go
--- Visualizer.py
+++ Visualizer.py
@@ -1,10 +1,10 @@
 from plotly.graph_objs import layout
 from BE import KNN
 import numpy as np
-from sklearn.model_selection import train_test_split
+#from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
-from sklearn.datasets import load_iris
+#from sklearn.datasets import load_iris
 import plotly.express as px
 import plotly.graph_objects as go
 
@@ -15,21 +15,21 @@ class Visualizer():
         print("\n")
 
         choosed = input("Type the name of the csv: ")
-        data_set = pd.read_csv("Datasets/"+choosed+".csv")
+        self.data_set = pd.read_csv("Datasets/"+choosed+".csv")
 
         self.independent_variable1 = int(input(
-            "Type the column position of the dependent variable 1 (x) (must be numeric) from left to right: "))-1
+            "Type the column position of the independent variable 1 (x) (must be numeric) from left to right: "))-1
         self.independent_variable2 = int(input(
-            "Type the column position of the dependent variable 2 (y) (must be numeric) from left to right: "))-1
+            "Type the column position of the independent variable 2 (y) (must be numeric) from left to right: "))-1
 
         dependent_variable = int(input(
-            "Type the column position of the independent variable (must be numeric) from left to right: "))-1
+            "Type the column position of the dependent variable (must be numeric) from left to right: "))-1
 
         # Verificar que el archivo sea .csv
 
         # Forma óptima
-        temp = data_set.values
-        x = data_set.iloc[:, [self.independent_variable1, self.independent_variable2]]
+
+        x = self.data_set.iloc[:, [self.independent_variable1, self.independent_variable2]]
         x = x.astype("float64")
         x = x.values
         self.X_whitout_normalization = x
@@ -41,12 +41,12 @@ class Visualizer():
 
         self.Xvar = (x-self.xmin)/(self.xmax-self.xmin)
 
-        y = data_set.iloc[:, dependent_variable]
+        y = self.data_set.iloc[:, dependent_variable]
         self.Yvar = y.factorize()[0]
 
         self.categories = y.unique()
 
-        self.knn.fit(self.Xvar, self.Yvar)
+        self.knn.fit(self.X_whitout_normalization, self.Yvar)
 
         # Tomar etiquetas posibles
         # self.tagColumn = y.factorize()[1]
@@ -54,14 +54,14 @@ class Visualizer():
 
         # Para tomar el nombre poner el índice en el tag column
 
-        self.vid1 = data_set.columns[self.independent_variable1] + ""
-        self.vid2 = data_set.columns[self.independent_variable2] + ""
-        self.dv1 = data_set.columns[dependent_variable] + ""
+        self.vid1 = self.data_set.columns[self.independent_variable1] + ""
+        self.vid2 = self.data_set.columns[self.independent_variable2] + ""
+        self.dv1 = self.data_set.columns[dependent_variable] + ""
 
 
-        df = data_set[[self.vid1, self.vid2, self.dv1]]
-        cols = df.columns
-        fig = px.scatter(df, x=self.vid1, y=self.vid2, color=self.dv1)
+        df = self.data_set[[self.vid1, self.vid2, self.dv1]]
+
+        fig = px.scatter(df, x=self.vid1, y=self.vid2, color= self.dv1)
 
 
         fig.show()
@@ -72,24 +72,20 @@ class Visualizer():
 
     def askData(self):
         self.dfR = self.choose_dataset()
-        k = int(input("How many neighbors: "))
+        k = int(input("How many neighbors do you want? "))
         self.knn.k = k
 
         self.position_x = float(input("Type the value of  feature 1 (x): "))
         self.position_y = float(input("Type the value of feature 2 (y): "))
 
         # The classes matrix works with [y, x] so i have to change the order of my array
-        x1 = self.position_x-self.xmin/(self.xmax-self.xmin)
-        x2 = self.position_y-self.xmin/(self.xmax-self.xmin)
+        x1 = self.position_x#= self.position_x-self.xmin/(self.xmax-self.xmin)
+        x2 = self.position_y#= self.position_y-self.xmin/(self.xmax-self.xmin)
 
         classesData = [x1, x2]
 
-        # The position works as usual, so i need it in the normal order
-        positionData = [x1, x2]
-        data = [classesData, positionData]
-
-        self.addDot(classesData, positionData)
-        return data
+        self.addDot(classesData)
+        return classesData
 
     def __init__(self, knn):
         self.knn = knn
@@ -109,19 +105,17 @@ class Visualizer():
         self.independent_variable2  = None
         self.position_x = None
         self.position_y = None
+        self.data_set = None
 
 
 
 
     def start(self):
         data = self.askData()
-        # Graphic
-
-        # Df = el dataset que carguen
         return data
 
     ##Borrar positionData
-    def addDot(self, classData, positionData):
+    def addDot(self, classData):
 
         # Para los cercanos
         vecinos = self.knn.get_k_nearest_neighboors(classData)
@@ -158,31 +152,27 @@ class Visualizer():
         detectedClass = self.categories[detectedClass]
 
         newDyc = {self.vid1: self.position_x, self.vid2: self.position_y, self.dv1: detectedClass}
-        self.dfR.append(newDyc,ignore_index=True)
-        #self.dfR.append(newDyc, ignore_index=True)
+
+        self.data_set.append(newDyc, ignore_index = True)
 
         fig2 = px.scatter(self.dfR, x=self.vid1, y=self.vid2, color=self.dv1)
-        fig2.show()
+        #fig2.show()
 
-
-        self.connect_dots(vecinos, positionData, fig2)
+        self.connect_dots(vecinos, classData, fig2)
 
 
     def connect_dots(self, neighbours_position, position_data, fig2):
-       
-
   
-        # Tenemos la posición, hay que tomarlo del arreglo con todos los datos para poder conectarlos
+        print(neighbours_position)
 
         for i in range(len(neighbours_position)):
             neighbour_dot_position = neighbours_position[i]
             neighbour_dot  = self.X_whitout_normalization[neighbour_dot_position]
-            old_dot_class = self.Yvar[neighbour_dot_position]
 
             neighbour_y = neighbour_dot[1]
             neighbour_x = neighbour_dot[0]
 
-            fig2.add_scatter(x=[neighbour_x, self.position_x], y=[neighbour_y, self.position_y], showlegend=False, line_color='black')
+            fig2.add_scatter(x=[neighbour_x, self.position_x], y=[neighbour_y, self.position_y], showlegend=False, line_color= "green")
 
 
         fig2.show()
@@ -192,4 +182,3 @@ if __name__ == '__main__':
     knn = KNN()
     vis = Visualizer(knn)
     data = vis.start()
-    #vis.addDot(data[0], data[1])
